2714de4719830f5eeb00b5a5cddd34ab0d5e86e7
Alperen Diricanlı <sphinx@Mac-mini.local>
2025-08-30 12:26:54 +0300

    hotfix

--- config.js
+++ config.js
@@ -3,33 +3,33 @@ module.exports = {
         generalChat: "1407441105422913666" // Genel sohbet kanalı
     },
     logChannels: {
-        warnLog: "1407441105422913661",    // Uyarı takip log kanalı
-        muteLog: "1407441105422913662",    // Mute takip log kanalı
-        banLog: "1407441105422913663",     // Ban takip log kanalı
-        kickLog: "1407441105422913664",    // Kick takip log kanalı
-        memberLog: "1407441105422913658",  // Üye bildirimleri log kanalı
-        deletedMessage: "1407441104571470036", // Silinen mesajlar log kanalı
-        editedMessage: "1407441104571470037",  // Düzenlenen mesajlar log kanalı
-        createdChannel: "1407441104571470038", // Oluşturulan kanal log kanalı
-        updatedChannel: "1407441104571470039", // Güncellenen kanal log kanalı
-        deletedChannel: "1407441104571470041", // Silinen kanal log kanalı
-        createdThread: "1407441104571470042", // Oluşturulan altbaşlık log kanalı
-        updatedThread: "1407441104571470043", // Güncellenen altbaşlık log kanalı
-        deletedThread: "1407441104860872796", // Silinen altbaşlık log kanalı
-        createdRole: "1407441104860872797", // Oluşturulan rol log kanalı
-        updatedRole: "1407441104860872798", // Güncellenen rol log kanalı
-        roleGived: "1407441104860872799", // Rol verildi log kanalı
-        roleRemoved: "1407441104860872800", // Rol alındı log kanalı
-        deletedRole: "1407441104860872801", // Silinen rol log kanalı
-        memberJoin: "1407441104860872802", // Üye katıldı log kanalı
-        memberLeave: "1407441104860872803", // Üye ayrıldı log kanalı
-        changedNick: "1407441104860872804", // Kullanıcı adı değişti log kanalı
-        serverSettingsUpdated: "1407441105187901672" // Sunucu ayarları güncellendi log kanalı
+        warnLog: "1407348241632329878",    // Uyarı takip log kanalı
+        muteLog: "1406335224316690442",    // Mute takip log kanalı
+        banLog: "1406335177621377115",     // Ban takip log kanalı
+        kickLog: "1406335239852261376",    // Kick takip log kanalı
+        memberLog: "1406954780072476682",  // Üye bildirimleri log kanalı
+        deletedMessage: "1406925512785395722", // Silinen mesajlar log kanalı
+        editedMessage: "1406925601331220592",  // Düzenlenen mesajlar log kanalı
+        createdChannel: "1406925817715494933", // Oluşturulan kanal log kanalı
+        updatedChannel: "1406927297428062331", // Güncellenen kanal log kanalı
+        deletedChannel: "1406926112449105940", // Silinen kanal log kanalı
+        createdThread: "1406925934296039474", // Oluşturulan altbaşlık log kanalı
+        updatedThread: "1406927446716190761", // Güncellenen altbaşlık log kanalı
+        deletedThread: "1406926237158604953", // Silinen altbaşlık log kanalı
+        createdRole: "1406926048641290250", // Oluşturulan rol log kanalı
+        updatedRole: "1406927785603235940", // Güncellenen rol log kanalı
+        roleGived: "1406926908242792478", // Rol verildi log kanalı
+        roleRemoved: "1406927003617071134", // Rol alındı log kanalı
+        deletedRole: "1406926359607115776", // Silinen rol log kanalı
+        memberJoin: "1406926493212217410", // Üye katıldı log kanalı
+        memberLeave: "1406926532907368478", // Üye ayrıldı log kanalı
+        changedNick: "1406926722645102593", // Kullanıcı adı değişti log kanalı
+        serverSettingsUpdated: "1406927987668160563" // Sunucu ayarları güncellendi log kanalı
     },
     roles: {
-        muted: "1407441104307093682", 
-        moderator: "1407441104307093685",
-        star: "1407441104290451457"
+        muted: "1407356511533142029", 
+        moderator: "1405890731591208980",
+        star: "1406955479124410481"
     },
     emojis: {
         info: "ℹ️",
